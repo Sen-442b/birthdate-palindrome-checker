e8a070c56945d74233d7b257fe2b1199f5211bc6
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-07-28 17:50:42 +0530

    base logic updated

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ console.log(monthDayYear);
 //mm-dd-yy (removing the initials of any year )
 const monthDayYearTwoArr  = monthDayYear.split(""); //second format of the date
 monthDayYearTwoArr.splice(4,2);// removing the initials
-const monthDayYear = monthDayYearArr.join("")
+const monthDayYearTwo = monthDayYearTwoArr.join("")
 
 /*
 monthDayYearArr.splice(4,2); //removing initials of an year 
@@ -37,19 +37,29 @@ console.log(monthDayYear);
 
 //d-mm-yyyy
 const oneDigitDayMonthYearArr = dateNum.split("");
+if(oneDigitDayMonthYearArr[0]==="0"){
 oneDigitDayMonthYearArr.splice(0,1);
+console.log(oneDigitDayMonthYearArr);
+}
+
 const oneDigitDayMonthYear=oneDigitDayMonthYearArr.join("");
 console.log(oneDigitDayMonthYear);
+console.log(`year month etc ${yearMonthDay} ,
+ day month etc${dayMonthYear},
+ month day year${monthDayYear}, 
+ month day year without ini ${monthDayYearTwo}
+ and one digit day ${oneDigitDayMonthYear}  `)
 
-const dateNumReverse = [...dateNum].reverse().join("");
 
 
-console.log(dateNumReverse)
 
-
-    if(dateNum ===dateNumReverse){
+    if(getDataBoolean(yearMonthDay)||
+       getDataBoolean(dayMonthYear)||
+       getDataBoolean(monthDayYear)||
+       getDataBoolean(monthDayYearTwo)||
+       getDataBoolean(oneDigitDayMonthYear)){
         console.log("noice");
-    };
+    }
 })
 
 
@@ -66,12 +76,29 @@ function arrayMove(arr, old_index, new_index) {
     return arr; // for testing
 };
 
-// returns [2, 1, 3]
-console.log(arrayMove([1, 2, 3], 0, 1));
 
-//^refer - https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
+
+function getReverseString(string){
+    if(string===""){
+        return"";
+    }else{
+        return getReverseString(string.substr(1))+ string.charAt(0);
+
+    }
+
+} //note- find out  how is it working with whitespace as well 
+function getDataBoolean(date){
+    return date ===getReverseString(date);
+
+}
+
+
+
+
 
 /*
  *Note to remember for me 
- 1)splice("") method mutates the  original array, whereas other methods like reverse(), join() and split()  stores result in a separate assigned variable 
+ 1)splice("") method mutates the  original array, whereas other methods like reverse(), join() and split()  stores result in a separate assigned variable
+ 2) substr() returns  the string after a specified index and charAt() returns the character at the specified index  
+ 3)^refer - https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
 */
